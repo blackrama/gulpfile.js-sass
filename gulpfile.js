009b3c0aa58ac5fdb5fa73afe45d7cdbadea6eb2
blackrama <blackrama13@gmail.com>
2019-07-15 00:37:18 +0300

    new version gulpfile with command run

--- gulpfile.js
+++ gulpfile.js
@@ -1,118 +1,93 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
+const gcmq = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const del = require('del');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
-const cssFiles = [
-
-	'./src/css/some.css',
-	'./src/css/other.css'
-
-];
-const jsFiles = [
-	'./src/js/lib.js',
-	'./src/js/some.js'
-];
+const del = require('del');
+const prettyHtml = require('gulp-pretty-html');
+const gulpif = require('gulp-if');
 
 
 
+// developer options
+const isSync = (process.argv.indexOf('--sync') !== -1);
+const isMinHtml = (process.argv.indexOf('--mh') !== -1);
+const isCleanCss = (process.argv.indexOf('--cc') !== -1);
+const isSoMap = (process.argv.indexOf('--sm') !== -1);
+const isGroupQuery = (process.argv.indexOf('--gq') !== -1);
+
+// function delete all files from [dist] folder
+function clear(){
+	return del('./dist/*');
+}
 
 
+function styles(){
+	return gulp.src('./app/css/style.scss')
+		.pipe(gulpif(isSoMap, sourcemaps.init()))
+    	.pipe(sass().on('error', sass.logError))
+    	.pipe(autoprefixer({
+           overrideBrowserslist: ['last 2 versions'],
+            cascade: true
+        }))
+    	.pipe(gulpif(isGroupQuery, gcmq()))
+    	.pipe(gulpif(isCleanCss, cleanCSS()))
+    	.pipe(gulpif(isSoMap, sourcemaps.write()))
+    	.pipe(gulp.dest('./dist/css'))
+    	.pipe(gulpif(isSync, browserSync.stream()));
+	}
 
-function styles() {
-	return gulp.src(cssFiles)
-			.pipe(concat('style.css'))
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
-				cascade: false
-			}))
-			.pipe(cleanCSS({
-				level: 2
-			}))
-			.pipe(gulp.dest('./build/css'))
-			.pipe(browserSync.stream());
-};
 
-function scripts() {
-	return gulp.src(jsFiles)
-			.pipe(concat('all.js'))
-			.pipe(uglify({
-				toplevel: true
-			}))
-			.pipe(gulp.dest('./build/js'))
-			.pipe(browserSync.stream());
+function js(){
+	return gulp.src('./app/js/**/*.js')
+		.pipe(gulp.dest('./dist/js/'))
+		.pipe(gulpif(isSync, browserSync.stream()));
+}
+function img(){
+	return gulp.src('./app/img/**/*')
+		.pipe(gulp.dest('./dist/img'))
+}
+function fonts(){
+	return gulp.src('./app/fonts/**/*')
+		.pipe(gulp.dest('./dist/fonts/'))
+}
+function html(){
+	return gulp.src('./app/*.pug')
+		.pipe(pug({
+   		// Your options in here.
+  			}))
+		.pipe(gulpif(isMinHtml,prettyHtml()))
+		.pipe(gulp.dest('./dist/'))
+		.pipe(gulpif(isSync, browserSync.stream()));
+}
+
+// watcher
+function watch(){
+	if(isSync){
+		browserSync.init({
+	        server: {
+	            baseDir: "./dist/",
+	        },
+	         tunnel: false,
+	         tunnel: "pekur"
+	    });
+	}
 	
-};
 
-
-
-function watch() {
-	  browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        // tunnel: true
-    });
-
-	// gulp.watch('./src/css/**/*.css',styles);
-	// gulp.watch('./src/scss/**/*.scss',cssSass);
-	gulp.watch('./src/scss/**/*.scss',sassprefixclean);
-	gulp.watch('./src/js/**/*.js',scripts);
-	gulp.watch('./src/*.pug',pughtml);
-	gulp.watch('./*.html',browserSync.reload);
+	gulp.watch('./app/css/**/*.scss', styles);
+	gulp.watch('./app/*.pug', html);
+	gulp.watch('./app/js/**/*.js',js)
 	
-};
+}
 
 
-function clean() {
-	return del(['build/*']);
-};
+let build = gulp.series(clear, 
+	gulp.parallel(styles, img, html,fonts, js)
+);
 
-
-function pughtml() {
-	return gulp.src('src/*.pug')
-	  .pipe(pug({
-	    // Your options in here.
-	  }))
-	  .pipe(gulp.dest('./'))
-	   .pipe(browserSync.stream());
-};
-function cssSass() {
-	return gulp.src('./src/scss/**/*.scss')
-			    .pipe(sass().on('error', sass.logError))
-			    .pipe(gulp.dest('./build/css'))
-			    .pipe(browserSync.stream());
-};
-
-function sassprefixclean(argument) {
-	return gulp.src('./src/scss/**/*.scss')
-			    .pipe(sass().on('error', sass.logError))
-				.pipe(autoprefixer({
-					browsers: ['last 2 versions'],
-					cascade: false
-				}))
-				.pipe(cleanCSS({
-					level: 2
-				}))
-			 	.pipe(gulp.dest('./build/css'))
-			    .pipe(browserSync.stream());
-};
-
-
-
-
-gulp.task('styles',styles);
-gulp.task('scripts',scripts);
-gulp.task('watch',watch);
-gulp.task('clean',clean);
-gulp.task('pughtml',pughtml);
-gulp.task('cssSass',cssSass);
-gulp.task('build', gulp.series(clean,
-						gulp.parallel(styles,scripts)
-						));
-gulp.task('dev', gulp.series('build','watch'));
-gulp.task('sassprefixclean',sassprefixclean);
+//  gulp tasks
+gulp.task('build', build);
+gulp.task('watch', gulp.series(build, watch));
